07c3d50f76c0fc23d3f84a2a4bcd86800c09989a
Jake Robbins <jrobbinsd@gmail.com>
2019-06-04 17:35:29 -0600

    header contains routing

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,9 @@
 <template lang="html">
   <div class="mainHeader">
+		<div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
 		<h2>header text goes here</h2>
 		<button type="button" name="button" @click='this.load'>click me</button>
   </div>
