9310015e25c63c2de5bb8bdd198d8d7c3ebe2459
Jake Robbins <jrobbinsd@gmail.com>
2019-06-04 17:20:13 -0600

    header page

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+  <div class="mainHeader">
+		<h2>header text goes here</h2>
+		<button type="button" name="button" @click='this.load'>click me</button>
+  </div>
+</template>
+
+<script>
+export default {
+	methods: {
+		load: function () {
+			console.log('hello')
+		}
+
+	}
+}
+</script>
+
+<style lang="css" scoped>
+.mainHeader {
+	height: 10vw;
+	background-color: teal;
+}
+h2 {
+	padding: 0px;
+}
+</style>
