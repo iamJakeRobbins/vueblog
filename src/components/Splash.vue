c2c725f4ece8611d521f6a71f26a8a6b844978c8
Jake Robbins <jrobbinsd@gmail.com>
2019-06-04 16:57:07 -0600

    compnent test

---
+++ src/components/Splash.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+	<div class="">
+		<h1>Landing Page test</h1>
+		<img src="../assets/logo.png" alt="">
+	</div>
+
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
