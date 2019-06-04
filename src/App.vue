15ecab2a01ca92d63ec6790b32c5268a14067acd
Jake Robbins <jrobbinsd@gmail.com>
2019-06-04 16:56:50 -0600

    splash component and initial com

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+		<Splash/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Splash from './components/Splash.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+		Splash
   }
 }
 </script>
