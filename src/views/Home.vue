476756230e5da54fa96f8461e07103c21ac01f84
Jake Robbins <jrobbinsd@gmail.com>
2019-06-04 17:20:50 -0600

    reformat landing page

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+		<Header/>
+		<Splash/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import Splash from '@/components/Splash.vue'
+import Header from '@/components/Header.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld,
+		Splash,
+		Header,
+  }
+}
+</script>
