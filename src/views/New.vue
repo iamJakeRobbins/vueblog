0320cac89cf74448e6bd6ae4a2d8fe64d4f0b105
Jake Robbins <jrobbinsd@gmail.com>
2019-06-04 20:14:06 -0600

    adding a new view that will add new entries

---
+++ src/views/New.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="new">
+		<Header/>
+    <h1>This is a new view</h1>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue';
+
+export default {
+  name: 'new',
+  components: {
+		Header,
+  }
+}
+</script>
