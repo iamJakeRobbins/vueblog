a6c49110beaf5d3b8c1b530c1419826d40490e25
Jake Robbins <jrobbinsd@gmail.com>
2019-06-05 16:34:00 -0600

    purge unused components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,14 +7,12 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 import Splash from '@/components/Splash.vue'
 import Header from '@/components/Header.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
 		Splash,
 		Header,
   }
