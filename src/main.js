2018b61c30b9d9567bd816f1c877ca4d34edc101
Jake Robbins <jrobbinsd@gmail.com>
2019-06-05 16:34:10 -0600

    purge unsed components

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import router from './router'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
-// import 'font-awesome'
 
 Vue.config.productionTip = false
 
