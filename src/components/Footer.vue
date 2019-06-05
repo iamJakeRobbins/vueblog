6d5ee635ab3610fdd709b253be4f77d1eec0d1ce
Jake Robbins <jrobbinsd@gmail.com>
2019-06-05 16:59:10 -0600

    added footer w/ basic functionality

---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+  <div id="footer">
+	<router-link to="new">
+		<strong>footer test</strong>
+	</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+#footer {
+	position: fixed;
+	left:0px;
+	bottom:0px;
+	height:30px;
+	width:100%;
+
+}
+</style>
