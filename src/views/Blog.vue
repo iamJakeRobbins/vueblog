e47fc2ed9002f24270bbb9b807ff427971568d4e
Jake Robbins <jrobbinsd@gmail.com>
2019-06-09 11:10:47 -0600

    summary link

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,11 +1,34 @@
 <template lang="html">
-  <h1>route working</h1>
+	<div id="blog">
+		<h1>route working</h1>
+		<Summaries/>
+	</div>
+
 </template>
 
 <script>
+import Summaries from '@/components/Summary.vue';
 export default {
+	data: function() {
+		return {
+			posts: {
+
+			}
+		}
+	},
+	components: {
+		Summaries,
+	}
 }
+
 </script>
 
 <style lang="css" scoped>
+#blog{
+	height: 100vh;
+	background-image: url('../assets/splash.jpg');
+	background-repeat: no-repeat;
+	background-size: 100vw 100vh;
+	color: white;
+}
 </style>
