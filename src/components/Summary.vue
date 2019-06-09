287b65fc3ff72fc95925c4f119c24b499e547d2b
Jake Robbins <jrobbinsd@gmail.com>
2019-06-09 11:10:21 -0600

    summary viewport for existing posts

---
+++ src/components/Summary.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+  <div id="summaryWindow">
+
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+#summaryWindow {
+	margin-left: 25vw;
+	margin-right: 25vw;
+	height: 90vh;
+	width: 50vw;
+	background-color: white;
+	overflow: scroll;
+}
+</style>
